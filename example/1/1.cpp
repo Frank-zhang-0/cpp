06589d09ef2fc88a6ba7da8897ce32b39ee27198
zhang14386 <1438692163@qq.com>
2023-09-07 22:28:08 +0800

    Hello World!

---
+++ example/1/1.cpp
@@ -0,0 +1,12 @@
+/*Hello World!--你好 世界！*/
+#include <iostream>
+int main()
+{
+    using namespace std;
+
+    cout << "Hello World!";
+    cout << endl;
+    cout << "你好 世界！" << endl;
+    
+    return 0;
+}
